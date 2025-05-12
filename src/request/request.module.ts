74dd46aabe2b141a07e1f5a961d6522e37fba7a7
Alimp <A.Mohsenpour@sgas.ir>
2025-05-12 17:43:41 +0330

    Update duplicate req

--- src/request/request.module.ts
+++ src/request/request.module.ts
@@ -4,9 +4,10 @@ import { RequestService } from './request.service';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestEntity } from 'src/entities/request.entity';
+import { FriendsEntity } from 'src/entities/friends.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RequestEntity])],
+  imports: [TypeOrmModule.forFeature([RequestEntity, FriendsEntity])],
   controllers: [RequestController],
   providers: [RequestService,
     {
--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { RequestEntity } from 'src/entities/request.entity';
-
+import { FriendsEntity } from 'src/entities/friends.entity';
 interface ICreateRequest {
   to: number;
   from: number;
@@ -12,6 +12,8 @@ export class RequestService {
   constructor(
     @InjectRepository(RequestEntity)
     private readonly requestRepository: Repository<RequestEntity>,
+    @InjectRepository(FriendsEntity)
+    private readonly friendsRepository: Repository<FriendsEntity>,
   ) {}
 
   async getRequests(id: number) {
@@ -19,12 +21,23 @@ export class RequestService {
   }
 
   async createRequest(body: ICreateRequest) {
+    const existDuplicated = await this.requestRepository.findOne({
+      where: { to: body.from, from: body.to },
+    });
     const existingRequest = await this.requestRepository.findOne({
       where: { to: body.to, from: body.from },
     });
-    if (existingRequest) {
+    const existingFriend = await this.friendsRepository.findOne({
+      where: { friend: body.to},
+    });
+    if (existingRequest || existDuplicated) {
       return {
-        message: 'Request Failed',
+        message: 'Request duplicated',
+        statusCode: 400,
+      };
+    } else if(existingFriend) {
+      return {
+        message: 'This user is your friend',
         statusCode: 400,
       };
     } else {
